29024404faa81d04e57b7ea817d195545fb59264
PC\Киев <sergefrontend@gmail.com>
2018-08-02 15:21:35 +0300

    vidimost сборка

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+
+	$('.home-page__slider').slick({
+		slidesToShow: 1,
+		autoPlay: true,
+		slidesToScroll: 1,
+		swipeToSlide: true,
+		dots: true,
+		arrows: true
+	});
+
+
+	
+});
