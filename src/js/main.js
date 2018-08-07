6193efaa0558abe941ccc153e4975b4cf343fb02
PC\Киев <sergefrontend@gmail.com>
2018-08-07 16:15:25 +0300

    страница главная

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,15 @@ $(document).ready(function() {
 		slidesToScroll: 1,
 		swipeToSlide: true,
 		dots: true,
-		arrows: true
+		arrows: true,
+		responsive: [
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false
+      }
+    }
+  ]
 	});
 
 	$('.open-menu').on('click', function() {
@@ -15,18 +23,41 @@ $(document).ready(function() {
 		$('.page-menu').toggleClass('page-menu--open');
 	});
 
+	var halfText = $('.about-sect__text').innerHeight() / 2,
+			textHeight = $('.about-sect__text').innerHeight();
+
+			$('.about-sect__text').css('height', $('.about-sect__text').innerHeight() /2);
+
+
 	$('.about-sect__more').on('click', function(event) {
 		event.preventDefault();
 		$('.about-sect__text').toggleClass('about-sect__text--clip');
-
-		if( $('.about-sect__text').hasClass('about-sect__text--clip') ) {
-			$(this).html('Читать дальше');
+		if( $('.about-sect__text').innerHeight() == halfText ) {
+			$('.about-sect__text').animate({
+				height: textHeight
+			}, 500);
+			$(this).text('Скрыть');
 		}
 
 		else {
-			$(this).html('Свернуть');
+			$('.about-sect__text').animate({
+				height: halfText
+			}, 500);
+			$(this).text('Читать дальше');
 		}
-	})
+
+		
+	});
+
+	if( window.matchMedia("(max-width: 991px)").matches) {
+		$('.submenu').hide();
+		$('.page-menu__item').on('click', function() {
+			$(this).children('.submenu').slideToggle();
+		});
+		$('.search-form__btn-product').on('click', function() {
+			$(this).children('.submenu').slideToggle();
+		});
+	}
 
 
 	
